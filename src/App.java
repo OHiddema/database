da7fd140837275d11b024ae59ebd26136e14cde6
Oege Hiddema <oegehiddema@gmail.com>
2022-06-08 16:51:33 +0200

    use try-with-resources

--- src/App.java
+++ src/App.java
@@ -6,16 +6,13 @@ public class App {
         String user = "root";
         String password = "root";
         String sql = "SELECT * FROM city where CountryCode='NLD'";
-        try {  
-            Connection con = DriverManager.getConnection(url, user, password);  
+        try (Connection con = DriverManager.getConnection(url, user, password)) {  
             Statement stmt = con.createStatement();  
             ResultSet rs = stmt.executeQuery(sql);  
             while(rs.next()) {
                 System.out.println(rs.getString(2));
             }
-            con.close();  
-        }
-        catch(Exception e) {
+        } catch(Exception e) {
             System.out.println(e);
         }
     }
