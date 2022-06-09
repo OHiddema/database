b922087304d3ea81dc45aea5ca740af28b0085aa
Oege Hiddema <oegehiddema@gmail.com>
2022-06-09 11:15:29 +0200

    use a more complex sql statement

--- src/App.java
+++ src/App.java
@@ -5,12 +5,13 @@ public class App {
         String url = "jdbc:mysql://localhost:3306/world";
         String user = "root";
         String password = "root";
-        String sql = "SELECT * FROM city where CountryCode='NLD'";
+        // String sql = "SELECT * FROM city where CountryCode='NLD'";
+        String sql = "SELECT world.countrylanguage.Language, round(sum(world.country.population * world.countrylanguage.percentage/100),0) as total from world.country inner join world.countrylanguage on country.Code = countrylanguage.CountryCode group by world.countrylanguage.language order by total desc;";
         try (Connection con = DriverManager.getConnection(url, user, password)) {  
             Statement stmt = con.createStatement();  
             ResultSet rs = stmt.executeQuery(sql);  
             while(rs.next()) {
-                System.out.println(rs.getString("Name"));
+                System.out.println(rs.getString(1) + " " + rs.getInt(2));
             }
         } catch(Exception e) {
             System.out.println(e);
