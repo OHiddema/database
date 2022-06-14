5065ffd770ef5d8351fe435c01c907b7cc569617
Oege Hiddema <oegehiddema@gmail.com>
2022-06-14 20:04:47 +0200

    testje

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ import java.sql.*;
 
 public class App {
     public static void main(String[] args) {
+        // test!
         String url = "jdbc:mysql://localhost:3306/world";
         String user = "root";
         String password = "root";
